d2288a0aa594e83d06d1c802ba05dad24b35e8c8
H_A _Opu <wwwhasib4@gmail.com>
2022-02-15 18:49:01 +0600

    penguin

---
+++ penguin.js
@@ -0,0 +1,4 @@
+document.getElementById('buy-now-btn').addEventListener('click',function(){
+    const heading = document.getElementById('head-title');
+     heading.innerText = 'i am penguin';  
+})
